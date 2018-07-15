88bd563895997367726486949e73adaa35c87baa
Conor Hennessy <c.hennessy@outlook.com>
2018-07-16 00:04:47 +0100

    Attempt to Challenfe 363
    Attempted to complete challenge 363, got a little bit of the way but didn't finish - there we are!

---
+++ src/Challenge_363.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+
+public class Challenge_363 {
+    public static boolean check(String wordIn) {
+        String word = wordIn.toLowerCase();
+        /*if (!word.contains("ie")) {
+            return false;
+        }if (word.contains("ei")) {
+            return false;
+        }*/
+        if (word.contains("cei")) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.println("Word to check... ");
+        String word = input.next();
+        System.out.println("check(\""+word+"\") => " + check(word));
+    }
+}
+
