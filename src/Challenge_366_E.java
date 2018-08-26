1321014f3e4035584575f82da10c84c62af7ba71
Conor Hennessy <9257022+conorhennessy@users.noreply.github.com>
2018-08-26 02:05:48 +0100

    Attempt to #366 [Easy] ]Bonus 1

--- src/Challenge_366_E.java
+++ src/Challenge_366_E.java
@@ -1,3 +1,9 @@
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
 public class Challenge_366_E {
     public static Boolean wordFunnel(String fWord, String sWord){
         for (int i = 0; i < fWord.length(); i++){
@@ -8,7 +14,22 @@ public class Challenge_366_E {
         }
         return false;
     }
-    public static void main(String[] args) {
+
+    public static List<String> bonus(String fileName, String word) throws FileNotFoundException {
+        List<String> wordList = new ArrayList<>();
+        Scanner input = new Scanner(new File(fileName));
+        while (input.hasNextLine()){
+            String cWord = input.nextLine();
+            for (int i = 0; i < cWord.length(); i++){
+                String newWord = cWord.substring(0, i) + cWord.substring(i + 1, cWord.length());
+                if (newWord.equals(word)) { wordList.add(word); }
+            }
+        }
+        return wordList;
+    }
+
+    public static void main(String[] args) throws FileNotFoundException {
         System.out.println(wordFunnel("aapple","apple"));
+        System.out.println(bonus("enable1.txt", "drsgoon"));
     }
 }
